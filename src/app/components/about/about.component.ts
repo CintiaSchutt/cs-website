00cb488da50e004a89d96c03aa14b4eba589cabe
Cintia <cintiaschutt@gmail.com>
2026-04-26 16:02:35 -0600

    update angular to 21

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [MatIconModule, MatDividerModule, CommonModule],
+  imports: [MatIconModule, MatDividerModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
